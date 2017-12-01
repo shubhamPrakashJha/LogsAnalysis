47712471faeb8fbea8e65ff62bb95babcd470708
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2017-12-01 14:11:14 +0530

    querying popular_articles table, storing the result,and displaying output

--- report.py
+++ report.py
@@ -14,7 +14,12 @@ cursor = connection.cursor()
 print("="*65)
 print("\n" + " "*20 + " NEWS ANALYSIS REPORT\n")
 
-
+# executing popular_articles query and storing the result table in result1
+cursor.execute("select * from popular_articles")
+results1 = cursor.fetchall()
+print("\n1. The most popular three articles of all time are:\n")
+for i in range(3):
+    print("\t"+str(i+1)+". \""+results1[i][0]+"\" -- "+str(results1[i][1])+" views")
 
 
 
